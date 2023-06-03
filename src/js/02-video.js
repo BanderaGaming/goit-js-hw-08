82ba599033468855202a2a8ec70eb1138f0406f6
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-06-03 18:44:50 +0200

    work in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-    width: 640
-});
-
-player.on('play', function() {
-    console.log('played the video!');
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const storageKey = "videoplayer-current-time";
+const currentTime = (data) => localStorage.setItem(storageKey,data.seconds);
+player.on("timeupdate",throttle(currentTime,1000));
+player.setCurrentTime(localStorage.getItem(storageKey));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const storageKey = "feedback-form-state";
+const form = document.querySelector("form");
+const emailInput = document.querySelector("input");
+const messageInput = document.querySelector("textarea");
+
+const saveValues = () => {
+    const markup = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem(storageKey, JSON.stringify(markup));
+};
+
+    const a = () => {
+        try {
+            JSON.parse(storageKey)
+        } catch (error) {
+            console.error(error.message);
+            console.error(error.name);
+        }
+    }
+
+
+form.addEventListener("input", saveValues)
+a()
