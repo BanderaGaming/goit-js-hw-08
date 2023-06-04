595c5cb0421d34aa0dfa60164fbdbbf14525e8c0
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-06-04 15:29:41 +0200

    done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,24 +4,42 @@ const storageKey = "feedback-form-state";
 const form = document.querySelector("form");
 const emailInput = document.querySelector("input");
 const messageInput = document.querySelector("textarea");
+const localValues = localStorage.getItem(storageKey);
 
-const saveValues = () => {
+const makeObject = () => {
     const markup = {
         email: emailInput.value,
         message: messageInput.value,
     };
-    localStorage.setItem(storageKey, JSON.stringify(markup));
+    return markup;
 };
 
-    const a = () => {
-        try {
-            JSON.parse(storageKey)
-        } catch (error) {
-            console.error(error.message);
-            console.error(error.name);
-        }
+const saveValues = () => {
+    localStorage.setItem(storageKey, JSON.stringify(makeObject()));
+};
+
+const setValues = () => {
+if (localValues === null) {
+    return;
+}
+    try {
+        const parsedValues = JSON.parse(localValues);
+        emailInput.value = parsedValues.email;
+        messageInput.value = parsedValues.message;
+    } catch (error) {
+        console.error(error.message);
     }
+};
 
+const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(makeObject());
+    localStorage.clear();
+    form.reset();
 
-form.addEventListener("input", saveValues)
-a()
+};
+    
+
+form.addEventListener("input",throttle(saveValues,500));
+form.addEventListener("submit", onSubmit);
+setValues();
