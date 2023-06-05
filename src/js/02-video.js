8561fc27fb1f23bb9b2b3a7be787f3e5387f02a9
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-06-05 09:34:19 +0200

    fix1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const storageKey = "videoplayer-current-time";
-const currentTime = (data) => localStorage.setItem(storageKey,data.seconds);
-player.on("timeupdate",throttle(currentTime,1000));
-player.setCurrentTime(localStorage.getItem(storageKey));
+const localValue = localStorage.getItem(storageKey);
+const takeFromLocal = () => {
+    if (localValue===null) {
+        return 0;
+    }
+    return localValue;
+};
+takeFromLocal()
+const currentTime = (data) => localStorage.setItem(storageKey, data.seconds);
+player.on("timeupdate", throttle(currentTime, 1000));
+player.setCurrentTime(takeFromLocal());
